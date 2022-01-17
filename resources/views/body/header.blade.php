22be98be80b96a69eb46989e9bd6f1844a6ee023
James Adakole <jambone.james82@gmail.com>
2022-01-17 23:10:59 +0100

    final clean up before backend logic

--- resources/views/body/header.blade.php
+++ resources/views/body/header.blade.php
@@ -1,46 +1,50 @@
 <header class="main_header_area">
-			<div class="top_header_area row m0">
-				<div class="container">
-					<div class="float-left">
-						<a href="tell:+18004567890"><i class="fa fa-phone" aria-hidden="true"></i> + (1800) 456 7890</a>
-						<a href="mainto:info@cakebakery.com"><i class="fa fa-envelope-o" aria-hidden="true"></i> info@cakebakery.com</a>
-					</div>
-					<div class="float-right">
-						<ul class="h_social list_style">
-							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-						</ul>
-						<ul class="h_search list_style">
-							{{-- <li class="shop_cart"><a href="#"><i class="lnr lnr-cart"></i></a></li> --}}
-							<li><a class="popup-with-zoom-anim" href="#test-search"><i class="fa fa-search"></i></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="main_menu_area">
-				<div class="container">
-					<nav class="navbar navbar-expand-lg navbar-light bg-light">
-						<a class="navbar-brand" href="index.html">
-						<img src="img/logo.png" alt=""> 
-						<img src="img/logo-2.png" alt="">
-						</a>
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="my_toggle_menu">
-                            	<span></span>
-                            	<span></span>
-                            	<span></span>
-                            </span>
-						</button>
-						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav mr-auto">
-								<li class="dropdown submenu active">
-									<a class="dropdown-toggle" data-toggle="dropdown" href="{{ route('cake.index') }}" role="button" aria-haspopup="true" aria-expanded="false">Home</a>
-								</li>
-								<li><a href="#cakes">Our Cakes</a></li>
-								{{-- <li><a href="menu.html">Menu</a></li> --}}
-								{{-- <li class="dropdown submenu">
+    <div class="top_header_area row m0">
+        <div class="container">
+            <div class="float-left">
+                <a href="tell:+18004567890"><i class="fa fa-phone" aria-hidden="true"></i> + (1800) 456 7890</a>
+                <a href="mainto:info@cakebakery.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>
+                    info@cakebakery.com</a>
+            </div>
+            <div class="float-right">
+                <ul class="h_social list_style">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+                <ul class="h_search list_style">
+                    {{-- <li class="shop_cart"><a href="#"><i class="lnr lnr-cart"></i></a></li> --}}
+                    <li><a class="popup-with-zoom-anim" href="#test-search"><i class="fa fa-search"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="main_menu_area">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="index.html">
+                    <img src="img/logo.png" alt="">
+                    <img src="img/logo-2.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="my_toggle_menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="dropdown submenu active">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="{{ route('cake.index') }}"
+                                role="button" aria-haspopup="true" aria-expanded="false">Home</a>
+                        </li>
+                        <li><a href="#cakes">Our Cakes</a></li>
+                        {{-- <li><a href="menu.html">Menu</a></li> --}}
+                        {{-- <li class="dropdown submenu">
 									<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About Us</a>
 									<ul class="dropdown-menu">
 										<li><a href="about-us.html">About Us</a></li>
@@ -48,9 +52,9 @@
 										<li><a href="testimonials.html">Testimonials</a></li>
 									</ul>
 								</li> --}}
-							</ul>
-							<ul class="navbar-nav justify-content-end">
-								{{-- <li class="dropdown submenu">
+                    </ul>
+                    <ul class="navbar-nav justify-content-end">
+                        {{-- <li class="dropdown submenu">
 									<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
 									<ul class="dropdown-menu">
 										<li><a href="service.html">Services</a></li>
@@ -68,7 +72,7 @@
 										<li><a href="comming-soon.html">Coming Soon page</a></li>
 									</ul>
 								</li> --}}
-								{{-- <li class="dropdown submenu">
+                        {{-- <li class="dropdown submenu">
 									<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
 									<ul class="dropdown-menu">
 										<li><a href="blog.html">Blog with sidebar</a></li>
@@ -76,7 +80,7 @@
 										<li><a href="single-blog.html">Blog details</a></li>
 									</ul>
 								</li> --}}
-								{{-- <li class="dropdown submenu">
+                        {{-- <li class="dropdown submenu">
 									<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Shop</a>
 									<ul class="dropdown-menu">
 										<li><a href="shop.html">Main shop</a></li>
@@ -85,10 +89,10 @@
 										<li><a href="checkout.html">Checkout Page</a></li>
 									</ul>
 								</li> --}}
-								<li><a href="contact.html">Contact Us</a></li>
-							</ul>
-						</div>
-					</nav>
-				</div>
-			</div>
-		</header>
+                        <li><a href="contact.html">Contact Us</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
